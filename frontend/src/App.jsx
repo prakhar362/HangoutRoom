58b9ea3457ff0f5cc3a542b314170d353e8ce3d9
prakhar362 <prakharshri2005@gmail.com>
2025-04-17 18:28:16 +0530

    tailwind setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -39,21 +37,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-amber-200" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1 className="text-3xl font-bold text-amber-300 underline">
+    Hello world!
+  </h1>
         <button onClick={sendMessageToServer}>Send Message to Server</button>
       </div>
 
