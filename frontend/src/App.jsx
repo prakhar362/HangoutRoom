7242b101910c540c5979770a9789e35c1711d4fb
prakhar362 <prakharshri2005@gmail.com>
2025-04-17 21:11:54 +0530

    shadcn and folder str done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,17 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [socket, setSocket] = useState(null);
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Connect to the server
-    const socketInstance = io("http://localhost:3000"); // Replace with your server's URL
-    setSocket(socketInstance);
-
-    // Listen for incoming messages
-    socketInstance.on("connect", () => {
-      console.log("Connected to the server with socket ID:", socketInstance.id);
-    });
-
-    socketInstance.on("message", (data) => {
-      setMessage(data);
-      console.log("Message from server:", data);
-    });
-
-    // Cleanup on component unmount
-    return () => {
-      socketInstance.disconnect();
-    };
-  }, []);
-
-  const sendMessageToServer = () => {
-    if (socket) {
-      socket.emit("message", { text: "Hello, Server!" });
-    }
-  };
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className="bg-amber-200" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <h1 className="text-3xl font-bold text-amber-300 underline">
     Hello world!
   </h1>
-        <button onClick={sendMessageToServer}>Send Message to Server</button>
-      </div>
-
-      {message && <p>Message from server: {message.text}</p>}
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <button className="px-4 py-2 text-sm font-medium rounded-b-sm bg-white text-black rounded transition-colors duration-300 hover:bg-black hover:text-white border border-amber-100"> 
+    Click me</button>
     </>
   );
 }
